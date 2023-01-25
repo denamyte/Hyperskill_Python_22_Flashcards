41298096d5952c3041ee77c10d3946a4f738d470
denys <denamyte@gmail.com>
2023-01-25 10:57:32 +0200

    2023-01-24: Stage 1/7: finished

--- Flashcards/task/flashcards/flashcards.py
+++ Flashcards/task/flashcards/flashcards.py
@@ -1,1 +1,20 @@
-# Write your code here
+class Card:
+    def __init__(self, term: str, definition: str):
+        self.term = term
+        self.definition = definition
+
+    def __str__(self):
+        return f"""\
+Card:
+{self.term}
+Definition:
+{self.definition}"""
+
+
+def main():
+    c = Card('purchase', 'buy')
+    print(c)
+
+
+if __name__ == '__main__':
+    main()
